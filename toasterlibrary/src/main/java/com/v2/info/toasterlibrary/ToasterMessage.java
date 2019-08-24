a908144196b38abe00f2354b519bb8d9b0f00f1e
aditya <aditya.v2info@gmail.com>
2019-08-24 17:09:52 +0530

    first commit

---
+++ toasterlibrary/src/main/java/com/v2/info/toasterlibrary/ToasterMessage.java
@@ -0,0 +1,18 @@
+package com.v2.info.toasterlibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ToasterMessage {
+    public static void s(Context c, String  message)
+    {
+        Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+
+    }
+    public static void l(Context c, String  message)
+    {
+        Toast.makeText(c,message,Toast.LENGTH_LONG).show();
+
+    }
+
+}
